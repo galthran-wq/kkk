594d413266f59b2a44801e655769e3727290a186
galthran-wq <alcaponeforum@gmail.com>
2022-05-01 14:14:56 +0300

    JWT adjustments; todo

--- server/service/src/main/java/com/resitplatform/application/TokenAssembler.java
+++ server/service/src/main/java/com/resitplatform/application/TokenAssembler.java
@@ -1,19 +1,15 @@
- 
 package com.resitplatform.application;
 
 import com.resitplatform.api.dto.UserDto;
-import com.resitplatform.application.service.JwtService;
 import com.resitplatform.domain.model.User;
 
-public class UserAssembler {
+public class TokenAssembler {
 
-    // Todo it should only output token; and the client would get the information by simply decoding it.
-    public static UserDto assemble(User user, JwtService jwtService) {
+    public static UserDto assemble(User user) {
         return UserDto.builder()
                 .email(user.getEmail())
                 .isTeacher(user.getIs_teacher())
                 .username(user.getUsername())
-                .token(jwtService.getToken(user))
                 .bio(user.getBio())
                 .image(user.getImage())
                 .build();
--- server/service/src/main/java/com/resitplatform/application/service/DefaultJwtService.java
+++ server/service/src/main/java/com/resitplatform/application/service/DefaultJwtService.java
@@ -1,6 +1,11 @@
  
 package com.resitplatform.application.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.resitplatform.api.dto.UserDto;
+import com.resitplatform.application.TokenAssembler;
+import com.resitplatform.application.UserAssembler;
 import com.resitplatform.domain.model.User;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
@@ -9,6 +14,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Calendar;
+import java.util.Date;
 
 @Service
 public class DefaultJwtService implements JwtService {
@@ -32,8 +39,26 @@ public class DefaultJwtService implements JwtService {
 
     @Override
     public String getToken(User user) {
+
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonUser = "";
+        try {
+            jsonUser = mapper.writeValueAsString(
+                    TokenAssembler.assemble(user)
+            );
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        Calendar c = Calendar.getInstance();
+        c.add(Calendar.MONTH, 1);
+        Date expiration = c.getTime();
+
         return Jwts.builder()
                 .setSubject(user.getUsername())
+                // todo should be subject; JwtFilter should parse jsonUser for username
+                .claim("temp", jsonUser)
+                .setExpiration(expiration)
                 .signWith(key)
                 .compact();
     }
